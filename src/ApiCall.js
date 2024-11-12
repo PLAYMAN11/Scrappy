a367a0b17a3e71b56b8d67788406c81dce4356fd
pepepecas12000 <jjimenechuyin@gmail.com>
2024-11-11 19:32:10 -0600

    lol no jala

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import mercadolibre from './ScrapMercadoLibre';
 import amazon from './BrightData';
 
+
+
+
 const APIsCall = () => {
     const [InputValue, setInputValue] = useState('');
     const [Loading, setLoading] = useState(false);
@@ -13,8 +16,12 @@ const APIsCall = () => {
         try {
 
             const mercadoResponse = await mercadolibre(InputValue);
+            console.log(mercadoResponse);
+            mercadoResponse=sortJSON(mercadoResponse,'desc')
+
             const amazonResponse = await amazon(InputValue);
 
+            console.log(mercadoResponse);
             // If the API response structure is not flat, you might need to access specific fields
             const data = {
                 Mercado_Libre: mercadoResponse?.data || mercadoResponse, // Use the correct data structure
@@ -28,6 +35,23 @@ const APIsCall = () => {
             setLoading(false);
         }
     };
+
+    function sortJSON(data, orden) {
+        return data.sort((a, b) => {
+            const x = parseFloat(a.price);
+            const y = parseFloat(b.price);
+
+            if (orden === 'asc') {
+                return x - y; // Orden ascendente
+            }
+
+            if (orden === 'desc') {
+                return y - x; // Orden descendente
+            }
+
+            return 0; // Si no se especifica un orden, no se realiza ningún cambio
+        });
+    }
 
     return (
         <div>
