05fa07813e04474d69e04c4391a52034c21e12b0
pepepecas12000 <jjimenechuyin@gmail.com>
2024-11-11 21:21:20 -0600

    no jala

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -2,7 +2,28 @@ import React, { useState } from 'react';
 import mercadolibre from './ScrapMercadoLibre';
 import amazon from './BrightData';
 
+function sortJSON(data, orden) {
+    return data.sort((a, b) => {
+        const x = parseFloat(a.price.replace(/,/g, '')) || 0;
+        const y = parseFloat(b.price.replace(/,/g, '')) || 0;
 
+
+        if (orden === 'asc') {
+            return x - y; // Orden ascendente
+        }
+
+        if (orden === 'desc') {
+            return y - x; // Orden descendente
+        }
+
+        return 0;
+    });
+}
+function Combine(JSON1, JSON2){
+    const result = {...JSON1, ...JSON2};
+console.log(result)
+    return result
+}
 
 
 const APIsCall = () => {
@@ -21,14 +42,8 @@ const APIsCall = () => {
 
             const amazonResponse = await amazon(InputValue);
 
-            console.log(mercadoResponse);
-            // If the API response structure is not flat, you might need to access specific fields
-            const data = {
-                Mercado_Libre: mercadoResponse?.data || mercadoResponse, // Use the correct data structure
-                Amazon: amazonResponse?.data || amazonResponse, // Adjust based on the response structure
-            };
 
-            setAPIsData(data);
+
         } catch (error) {
             console.error("Error fetching data:", error);
         } finally {
@@ -36,22 +51,7 @@ const APIsCall = () => {
         }
     };
 
-    function sortJSON(data, orden) {
-        return data.sort((a, b) => {
-            const x = parseFloat(a.price);
-            const y = parseFloat(b.price);
 
-            if (orden === 'asc') {
-                return x - y; // Orden ascendente
-            }
-
-            if (orden === 'desc') {
-                return y - x; // Orden descendente
-            }
-
-            return 0; // Si no se especifica un orden, no se realiza ningún cambio
-        });
-    }
 
     return (
         <div>
